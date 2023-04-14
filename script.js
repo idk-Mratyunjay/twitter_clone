563eaeaf9fdc9ec9db4fe5f5f2c54350d7a42569
Mruthunjay <idk.mratyunjay@gmail.com>
2023-04-14 20:19:14 +0530

    html and css

---
+++ script.js
@@ -0,0 +1,44 @@
+// DOM elements
+
+
+const mainPage = document.querySelector('.main-page');
+const loginPage = document.querySelector('.login-page');
+const middleContent = document.querySelector('.middle-content');
+
+
+/* *******************************  */
+
+// Main page 
+
+const goToLoginPage = () => {
+    mainPage.style.display = "none";
+    loginPage.style.display = "grid";
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
